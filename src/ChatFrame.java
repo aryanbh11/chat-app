fff7e70ccaed76ff6ed606efed75e1b1e47c9ae8
Aryan Bhatia <aryanbhatia14@gmail.com>
2020-12-28 05:08:40 +1100

    Basic GUI and Connection Established

---
+++ src/ChatFrame.java
@@ -0,0 +1,100 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ChatFrame extends JFrame implements ActionListener {
+    
+    private JPanel chatPanel;
+    private JPanel contactPanel;
+
+    private JLabel talkingTo;
+    private JLabel activeNow;
+    
+    private JButton sendButton;
+    
+    private JScrollPane textPane;
+    private JScrollPane messagePane;
+
+    private JTextArea textArea;
+    private JTextArea messageBox;
+
+    private final Font bf = new Font(Font.SANS_SERIF, Font.BOLD, 17);
+    private final Font f = new Font(Font.SANS_SERIF, Font.ITALIC, 10);
+
+    private final static int WIDTH = 400;
+    private final static int HEIGHT = 800;
+
+    public ChatFrame(String from, String to, boolean centre) {
+        chatPanel = new JPanel();
+        chatPanel.setLayout(null);
+        chatPanel.setBackground(new Color(233, 219, 232));
+        chatPanel.setBounds(1, 50, WIDTH, HEIGHT - 50);
+        chatPanel.setVisible(true);
+
+        contactPanel = new JPanel();
+        contactPanel.setLayout(null);
+        contactPanel.setBackground(new Color(137, 41, 133));
+        contactPanel.setBounds(1, 1, WIDTH, 50);
+        contactPanel.setVisible(true);
+
+        talkingTo = new JLabel(to);
+        talkingTo.setFont(bf);
+        talkingTo.setForeground(Color.WHITE);
+        talkingTo.setBounds(10, 1, 200,40);
+        contactPanel.add(talkingTo);
+
+        activeNow = new JLabel("Active Now");
+        activeNow.setFont(f);
+        activeNow.setForeground(Color.WHITE);
+        activeNow.setBounds(10, 35, 100,10);
+        contactPanel.add(activeNow);
+        activeNow.setVisible(false);
+
+        textArea = new JTextArea(5, 20);
+        textArea.setBounds(1, 1, WIDTH, HEIGHT - 200);
+        textArea.setBackground(new Color(233, 219, 232));
+        textArea.setText("Sample");
+        textArea.setEditable(false);
+        textPane = new JScrollPane(textArea);
+        textPane.setBounds(1, 1, WIDTH, HEIGHT - 200);
+        textPane.setBackground(new Color(233, 219, 232));
+        chatPanel.add(textPane);
+
+        messageBox = new JTextArea(5, 20);
+        messageBox.setText("Enter Message");
+        messageBox.setBounds(1, HEIGHT - 200, WIDTH - 50, 200);
+        messagePane = new JScrollPane(messageBox);
+        messagePane.setBounds(1, HEIGHT - 200, WIDTH - 50, 200);
+        chatPanel.add(messagePane);
+        
+        sendButton = new JButton("Send");
+        sendButton.setOpaque(true);
+        sendButton.setBounds(WIDTH - 50, HEIGHT - 200, 50, 20);
+        sendButton.addActionListener(this);
+        chatPanel.add(sendButton);
+
+        setTitle(from);
+        setSize(WIDTH, HEIGHT);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        add(chatPanel);
+        add(contactPanel);
+
+        if (centre) {
+            this.setLocationRelativeTo(null);
+        }
+        this.setVisible(true);
+    }
+
+    public void setActive(boolean active) {
+        activeNow.setVisible(active);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 
-}
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        ChatFrame clientChatFrame = new ChatFrame("Client", "Server", true);
+        Socket socket = new Socket("127.0.0.1", 9999);
+        System.out.println("Connected to Server!");
 
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream();
+
+        // output.write();
+
+        byte[] response = new byte[100];
+        input.read(response);
+        String received = new String(response).trim();
+        System.out.println("Received message from server: " + received);
+        
+        socket.close();
+    }
+
+}
